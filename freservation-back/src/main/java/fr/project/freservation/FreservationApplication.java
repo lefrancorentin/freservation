18a97820053a27ee02dc82cb0cc7814cbfb0e5a8
corentin_lefranc <lefrancorentin@gmail.com>
2023-06-18 18:19:26 +0200

    First commit - Initialisation du back

---
+++ freservation-back/src/main/java/fr/project/freservation/FreservationApplication.java
@@ -0,0 +1,14 @@
+package fr.project.freservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreservationApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hey");
+		SpringApplication.run(FreservationApplication.class, args);
+	}
+
+}
---
+++ freservation-back/src/test/java/fr/project/freservation/FreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.project.freservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
