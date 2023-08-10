25a2913c3c78844bc2e3873bb983d022c1731199
lefrancorentin <lefrancorentin@gmail.com>
2023-08-10 15:05:35 +0200

    Ajout d'un POM parent + renommage du groupId (#6)
    * Renommage groupId
    
    * Suppression fichier inutile
    
    * Création d'un pom parent
    
    * Commentaire + README

---
+++ freservation-back/README.md
@@ -0,0 +1,6 @@
+### Documentation
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.7.12/maven-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.7.12/maven-plugin/reference/html/#build-image)
+* [Spring REST Docs](https://docs.spring.io/spring-restdocs/docs/current/reference/html5/)
--- freservation-back/src/main/java/fr/korantun/freservation/controller/BookingController.java
+++ freservation-back/src/main/java/fr/korantun/freservation/controller/BookingController.java
@@ -1,4 +1,4 @@
-package fr.project.freservation.controller;
+package fr.korantun.freservation.controller;
 
 import javax.validation.Valid;
 
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import fr.project.freservation.model.BookingRequest;
+import fr.korantun.freservation.model.BookingRequest;
 
 @RestController
 // à mettre à un plus haut niveau ? sinon je répète /api pour tous mes controllers
