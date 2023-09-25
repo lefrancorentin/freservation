46ae5d2ba15d161f862439de3b726b76307430ad
lefrancorentin <lefrancorentin@gmail.com>
2023-09-25 13:30:17 +0200

    Ajout d'un bean de configuration pour logger les requêtes http entrantes (#8)

---
+++ freservation-back/src/main/java/fr/korantun/freservation/config/Logging.java
@@ -0,0 +1,19 @@
+package fr.korantun.freservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class Logging {
+
+    @Bean
+    CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter requestLoggingFilter = new CommonsRequestLoggingFilter();
+        requestLoggingFilter.setIncludeClientInfo(true);
+        requestLoggingFilter.setIncludeQueryString(true);
+        requestLoggingFilter.setIncludePayload(true);
+        requestLoggingFilter.setMaxPayloadLength(65536);
+        return requestLoggingFilter;
+    }
+}
