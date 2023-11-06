8eb73c9e14cb2e4b6ad676769c971eb4ef588ad9
corentin_lefranc <lefrancorentin@gmail.com>
2023-11-06 19:27:25 +0100

    Ajout composant de listing

---
+++ freservation-front/src/app/bookings-list/bookings-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-bookings-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bookings-list.component.html',
+  styleUrls: ['./bookings-list.component.scss']
+})
+export class BookingsListComponent {
+
+}
