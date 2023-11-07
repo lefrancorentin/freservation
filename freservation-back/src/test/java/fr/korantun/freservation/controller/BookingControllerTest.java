0ad4d0256cb7f47e996c4bd4e9d43cd397638e34
corentin_lefranc <lefrancorentin@gmail.com>
2023-11-07 17:09:06 +0100

    Fix TU

--- freservation-back/src/test/java/fr/korantun/freservation/controller/BookingControllerTest.java
+++ freservation-back/src/test/java/fr/korantun/freservation/controller/BookingControllerTest.java
@@ -1,24 +1,37 @@
 package fr.korantun.freservation.controller;
 
-import java.time.LocalDateTime;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
+import fr.korantun.freservation.data.BookingsRepository;
 import fr.korantun.freservation.model.BookingRequest;
 import fr.korantun.freservation.model.LanguageLevel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDateTime;
+
+@ExtendWith(MockitoExtension.class)
 class BookingControllerTest {
 
-    BookingController bookingController = new BookingController();
+    @Mock
+    BookingsRepository bookingsRepository;
+
+    @InjectMocks
+    BookingController bookingController;
 
     @Test
     void whenValidBookingRequestIsPassed_thenResponseHasCode200() {
         // given
-        BookingRequest emptyRequest = constructBookingRequest();
+        BookingRequest bookingRequest = constructBookingRequest();
 
         // when
-        ResponseEntity<String> httpResponse = bookingController.bookTimeSlot(emptyRequest);
+        Mockito.when(bookingsRepository.save(bookingRequest)).thenReturn(bookingRequest);
+
+        ResponseEntity<String> httpResponse = bookingController.bookTimeSlot(bookingRequest);
 
         //then
         Assertions.assertEquals(200, httpResponse.getStatusCodeValue());
